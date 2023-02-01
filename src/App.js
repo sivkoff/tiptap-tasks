263be9c3bfd817df1362de1a997f627a69e34e9b
Vitaly Sivkov <vitaly.sivkov@gmail.com>
2023-02-01 12:43:14 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import "./styles.scss";
+
+import Document from "@tiptap/extension-document";
+import Paragraph from "@tiptap/extension-paragraph";
+import TaskItem from "@tiptap/extension-task-item";
+import TaskList from "@tiptap/extension-task-list";
+import Text from "@tiptap/extension-text";
+import { EditorContent, useEditor } from "@tiptap/react";
+import React from "react";
+
+const CustomTaskItem = TaskItem.extend({
+  content: "inline*",
+  nested: true
+});
+
+export default () => {
+  const editor = useEditor({
+    extensions: [Document, Paragraph, Text, TaskList, CustomTaskItem],
+    content: `
+      <ul data-type="taskList">
+        <li data-type="taskItem" data-checked="true">flour</li>
+        <li data-type="taskItem" data-checked="true">baking powder</li>
+        <li data-type="taskItem" data-checked="true">salt</li>
+        <li data-type="taskItem" data-checked="false">sugar</li>
+        <li data-type="taskItem" data-checked="false">milk</li>
+        <li data-type="taskItem" data-checked="false">eggs</li>
+        <li data-type="taskItem" data-checked="false">butter</li>
+      </ul>
+    `
+  });
+
+  return <EditorContent editor={editor} />;
+};
